25695e3881e57b908fcc6c2ef07720ac0c815ab7
Kyuhyun Cho <wrg0731@gmail.com>
2022-11-18 19:50:53 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = -1;
+    int b = -1;
+
+    while (true) {
+        cin >> a >> b;
+        if (a == 0 && b == 0) {
+            break;
+        }
+        cout << a + b << '\n';
+    }
+}
