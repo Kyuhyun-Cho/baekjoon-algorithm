1f0f0aecf1159f2c21a5f4c8b91fa523703c3006
Kyuhyun Cho <wrg0731@gmail.com>
2022-11-18 19:09:19 +0900

    [Silver IV] Title: 큐, Time: 344 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int q[10001];
+int front_ = 0;
+int rear_ = 0;
+
+void push(int q[], int data) {
+    q[rear_] = data;
+    rear_++;
+}
+
+int pop(int q[]) {
+    if (front_ == rear_) {
+        return -1;
+    }
+    int tmp = q[front_];
+    front_++;
+    return tmp;
+}
+
+int size() {
+    return rear_ - front_;
+}
+
+int empty() {
+    if (front_ == rear_) {
+        return 1;
+    }
+    return 0;
+}
+
+int front(int q[]) {
+    if (front_ == rear_) {
+        return -1;
+    }
+    return q[front_];
+}
+
+int back(int q[]) {
+    if (front_ == rear_) {
+        return -1;
+    }
+    return q[rear_-1];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i< n; i++) {
+        string inst;
+        cin >> inst;
+
+        if (inst == "push") {
+            int x;
+            cin >> x;
+
+            push(q, x);
+        }
+
+        else if(inst == "pop") {
+            cout << pop(q) << '\n';
+        }
+
+        else if(inst == "size") {
+            cout << size() << '\n';
+        }
+            
+        else if(inst == "empty") {
+            cout << empty() << '\n';
+        }
+            
+        else if(inst == "front") {
+            cout << front(q) << '\n';
+        }
+
+        else if(inst == "back") {
+            cout << back(q) << '\n';
+        }
+    }
+}
