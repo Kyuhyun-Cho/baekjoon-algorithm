85897e74fbe292b1c83cf3f27d939548dbbc5a9f
Kyuhyun Cho <wrg0731@gmail.com>
2022-12-28 02:23:18 +0900

    [Silver I] Title: 정수 삼각형, Time: 44 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -8,22 +8,20 @@ int main() {
 	cin >> n;
 
 	int board[n][n]{};
-	int dp[n][n]{};
 	
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j <= i; j++) {
 			cin >> board[i][j];
-			dp[i][j] = board[i][j];
 		}
 	}
 
 	for (int i = n-1; i > 0; i--) {
 		for (int j = 0; j < i; j++) {
-			dp[i-1][j] = dp[i-1][j] + max(dp[i][j], dp[i][j+1]);
+			board[i-1][j] = board[i-1][j] + max(board[i][j], board[i][j+1]);
 		}
 	}
 	
-	cout << dp[0][0] << '\n';
+	cout << board[0][0] << '\n';
 
 	return 0;
 }
