0224898a5e74758a14668c6dc6b06e5552147385
Kyuhyun Cho <wrg0731@gmail.com>
2023-01-04 20:05:15 +0900

    [Silver IV] Title: 상근이의 여행, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int T, N, M, a, b;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        cin >> N >> M;
+        for (int i = 0; i < M; i++) {
+            cin >> a >> b;
+
+        }
+        cout << N - 1 << '\n';
+    }
+}
