bed33ecbeb99425d34fd4c46ab916123bd720887
Kyuhyun Cho <wrg0731@gmail.com>
2023-01-11 17:00:02 +0900

    [Silver III] Title: N과 M (5), Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[9];
+int num[9];
+bool visited[9];
+
+void dfs(int v) {
+	if (v == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		if(!visited[i]) {
+			visited[i] = true;
+			arr[v] = num[i];
+			dfs(v+1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> num[i];
+    }
+    sort(num+1, num+n+1);
+  
+	dfs(0);
+}
