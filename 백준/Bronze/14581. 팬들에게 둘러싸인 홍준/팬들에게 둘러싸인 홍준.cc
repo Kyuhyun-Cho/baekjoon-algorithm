65aa6b89ddea60790054f1c3d74a1cc06a9a55ec
Kyuhyun Cho <wrg0731@gmail.com>
2023-01-27 00:42:49 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string name; cin >> name;
+
+	cout << ":fan::fan::fan:" << '\n' << ":fan::" << name << "::fan:" << '\n' << ":fan::fan::fan:";
+}
