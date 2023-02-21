21b3c0c0aaa68315c948db348651bd5c01227d50
Kyuhyun Cho <wrg0731@gmail.com>
2023-02-21 23:12:45 +0900

    [Silver V] Title: 이장님 초대, Time: 16 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/9237. 이장님 초대/이장님 초대.cc
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.cc
@@ -1,24 +1,33 @@
 #include <iostream>
+#include <queue>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    int t[n];
-    for (int i = 0; i < n; i++) {
-        cin >> t[i];
-    }
-    sort(t, t + n);  // 각 나무가 다 자라는데 필요한 일수를 오름차순으로 정렬
+bool cmp(int a, int b) {
+	return a > b;
+}
 
-    int max_day = 0;  // 가장 큰 값을 저장할 변수
-    for (int i = 0; i < n; i++) {
-        int day = t[i] + n - i;  // 각 나무가 자라는데 필요한 일수와 나무를 심는 날짜를 더한 값
-        if (max_day < day) {
-            max_day = day;  // 가장 큰 값을 갱신
-        }
-    }
-    cout << max_day + 1 << endl;  // 초대하는 날짜는 가장 큰 값에 1을 더한 값
-    return 0;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n; cin >> n;
+	int arr[100001];
+	
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr+n, cmp);
+	for (int i = 1; i <= n; i++) {
+		arr[i-1] += i;
+	}
+	int max = -1;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	cout << max + 1 << '\n';
 }
