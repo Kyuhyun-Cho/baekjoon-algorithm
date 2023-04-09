ca35e9013d21ded0233e0aeab6eff6148cfb2054
Kyuhyun Cho <wrg0731@gmail.com>
2023-04-09 15:22:53 +0900

    [Bronze V] Title: The World Responds, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cout << "The world says hello!" << '\n';
+}
