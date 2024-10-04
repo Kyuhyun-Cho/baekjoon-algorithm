d612542a1ebc63cccc6ed748521cddf0118ccfb0
Kyuhyun Cho <wrg0731@gmail.com>
2024-10-04 23:35:58 +0900

    [Silver II] Title: 랜선 자르기, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,32 +1,33 @@
 import sys
 
-#sys.stdin = open("input.txt")
-
 k, n = map(int, sys.stdin.readline().split())
 
 lst = []
 
-for _ in range(k):
-    lst.append(int(sys.stdin.readline().rstrip()))
+for i in range(k):
+    tmp = int(sys.stdin.readline().rstrip())
+    lst.append(tmp)
 
-start = 1
-end = max(lst)
+left = 1
+right = max(lst)
+mid = (left+right)//2
 
 answer = 0
 
-while start <= end:
-    mid = (start+end)//2
+while left <= right:
+    mid = (left+right)//2
 
     cnt = 0
 
-    for i in range(k):
-        cnt += lst[i]//mid
+    for length in lst:
+        cnt += length//mid
 
     if cnt >= n:
-        start = mid + 1
         answer = mid
+        left = mid + 1
 
     else:
-        end = mid - 1
+        right = mid - 1
 
+    
 print(answer)
